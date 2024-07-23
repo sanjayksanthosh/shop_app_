700b26a37670a9fdfe98a3c4637ecdd123bcd8d2
sanjayksanthosh <sanjayksanthosh55@gmail.com>
2024-07-23 08:16:55 +0530

    category

--- lib/views/categoryPage.dart
+++ lib/views/categoryPage.dart
@@ -1,27 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shoop_app/model/categoryModel.dart';
+import 'package:shoop_app/views/categoryItemsPage.dart';
 
-class Categorypage extends StatelessWidget {
+class Categorypage extends StatefulWidget {
   Categorypage({super.key});
-  List Categories = [
-    "Vegitables",
-    "Fruits",
-    "Wellness",
-    "Electronics",
-  ];
+
+  @override
+  State<Categorypage> createState() => _CategorypageState();
+}
+
+class _CategorypageState extends State<Categorypage> {
+  List Categories = [];
+
+  getCategories() async {
+    Uri url = Uri.parse("https://node-server-ymb5.onrender.com/category");
+    var response = await http.get(url);
+    var data = categoryFromJson(response.body);
+    setState(() {
+      Categories = data;
+    });
+  }
+
+  @override
+  void initState() {
+    getCategories();
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
-        return Container(
-          padding: EdgeInsets.all(10),
-          margin: EdgeInsets.all(10),
-          height: 50,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(color: Colors.black)),
-          child: Text(
-            Categories[index],
-            style: TextStyle(fontSize: 25),
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(
+              builder: (context) {
+                return Categoryitemspage(
+                  categoryData: Categories[index],
+                );
+              },
+            ));
+          },
+          child: Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            height: 50,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Colors.black)),
+            child: Text(
+              Categories[index].name,
+              style: TextStyle(fontSize: 25),
+            ),
           ),
         );
       },
